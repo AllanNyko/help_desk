1eb2179ce5aeed20e4440a1cbe5cd41bfb6dd986
Allan Nyko <allanguimaraes@outlook.pt>
2023-10-26 15:17:03 -0300

    Listagem de chamados concluída

--- salvar_chamado.php
+++ salvar_chamado.php
@@ -5,3 +5,12 @@ echo '<pre>';
 print_r($_POST);
 
 echo '</pre>';
+
+$titulo = str_replace('#', '-', $_POST['titulo']);
+$categoria = str_replace('#', '-', $_POST['categoria']);
+$descricao = str_replace('#', '-', $_POST['descricao']);
+
+$arquivoDB = fopen('chamados.txt', 'a');
+$textoNovo = $titulo . '#' . $categoria . '#' . $descricao . PHP_EOL;
+fwrite($arquivoDB, $textoNovo);
+fclose($arquivoDB);
